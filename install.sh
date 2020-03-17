08d2f1ba3cc33ca1731da6fafe9e7d35a10bf681
Christophe Pollet <cpollet@users.noreply.github.com>
2020-03-17 19:29:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-git clone https://github.com/cpollet/gitconfig.git .gitconfig.git
-
-ln -s .gitconfig.git/gitconfig .gitconfig
-ln -s .gitconfig.git/gitignore .gitignore
+git clone https://github.com/cpollet/dotfiles.git .dotfiles.git
