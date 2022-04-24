0a4bd98b3ffad492ef572e0018436439b6cc712a
Christophe Pollet <cpollet@users.noreply.github.com>
2022-04-24 15:44:44 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,49 +1,86 @@
 #!/bin/bash
 
-git clone https://github.com/cpollet/dotfiles.git ~/.dotfiles.git
+[[ ! -d ~/.dotfiles.git ]] && \
+	git clone https://github.com/cpollet/dotfiles.git ~/.dotfiles.git
 
 echo "Install zsh ..."
-echo "When in zsh, hit CTRL+D to continue."
-echo "Press enter to continue"
-read
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mkdir -p ~/.oh-my-zsh/custom/themes
-ln -s ~/.dotfiles.git/files/zsh/cpollet.zsh-theme ~/.oh-my-zsh/custom/cpollet.zsh-theme
-sed -i 's/robbyrussell/cpollet/g' ~/.zshrc
-source ~/.zshrc
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting)/g' .zshrc
+command -v zsh 2>&1 >/dev/null
+if [[ $? -ne 0 ]]; then
+	echo "When in zsh, hit CTRL+D to continue."
+	echo "Press enter to continue"
+	read
+fi
+
+echo "Install oh-my-zsh ..."
+if [[ ! -d ~/.oh-my-zsh ]]; then
+	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	mkdir -p ~/.oh-my-zsh/custom/themes
+	ln -s ~/.dotfiles.git/files/zsh/cpollet.zsh-theme ~/.oh-my-zsh/custom/cpollet.zsh-theme
+	sed -i 's/robbyrussell/cpollet/g' ~/.zshrc
+fi
+
+[[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]] && \
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+sed -i 's/^plugins=.*$/plugins=(git zsh-syntax-highlighting)/g' ~/.zshrc
 
 echo "Install tmux ..."
 mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s ~/.dotfiles.git/files/tmux/tmux.conf ~/.tmux.conf
+
+[[ ! -d ~/.tmux/plugins/tpm ]] && \
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+[[ ! -f ~/.tmux.conf ]] && \
+	ln -s ~/.dotfiles.git/files/tmux/tmux.conf ~/.tmux.conf
 
 echo "Install git ..."
-ln -s .dotfiles.git/files/git/gitignore ~/.gitignore
-ln -s .dotfiles.git/files/git/gitconfig ~/.gitconfig
+[[ ! -f ~/.gitignore ]] && \
+	ln -s .dotfiles.git/files/git/gitignore ~/.gitignore
+[[ ! -f ~/.gitconfig ]] && \
+	ln -s .dotfiles.git/files/git/gitconfig ~/.gitconfig
 
-echo "Install aliases ..."
-echo -e "\n. ~/.dotfiles.git/files/all" >> ~/.zshrc
-
-echo "Install terminator ..."
-mkdir -p ~/.config/terminator
-ln -s ~/.dotfiles.git/files/terminator/config ~/.config/terminator
-
-echo "Install vim..."
-ln -s ~/.dotfiles.git/files/vim/vimrc ~/.vimrc
-
-echo "Install dracula theme"
-mkdir -p ~/.dracula
-command -v gnome-terminal 2>&1 >/dev/null
-if [[ $? -eq 0 ]]; then
-	git clone https://github.com/dracula/gnome-terminal ~/.dracula/gnome-terminal
-	~/.dracula/gnome-terminal/install.sh
-	echo '[ -f ~/.dir_colors/dircolors ] && eval `dircolors ~/.dir_colors/dircolors`' >> ~/.zshrc
-	rm ~/dircolors
+if [[ `grep "source ~/.dotfiles.git/files/all" ~/.zshrc | wc -l` = "0" ]]; then
+	echo "Install aliases ..."
+	echo -e "\nsource ~/.dotfiles.git/files/all" >> ~/.zshrc
 fi
+
 command -v terminator 2>&1 >/dev/null
 if [[ $? -eq 0 ]]; then
+	echo "Install terminator ..."
+	mkdir -p ~/.config/terminator
+	[[ ! -f ~/.config/terminator/config ]] && \
+		ln -s ~/.dotfiles.git/files/terminator/config ~/.config/terminator/config
+fi
+
+echo "Install vim..."
+[[ ! -f ~/.vimrc ]] && \
+	ln -s ~/.dotfiles.git/files/vim/vimrc ~/.vimrc
+vim +'PlugInstall --sync' +qa
+
+echo "Install dracula theme ..."
+mkdir -p ~/.dracula
+
+command -v gnome-terminal 2>&1 >/dev/null
+if [[ $? -eq 0 ]]; then
+	echo "  gnome-terminal ..."
+	[[ ! -d ~/.dracula/gnome-terminal ]] && \
+		git clone https://github.com/dracula/gnome-terminal ~/.dracula/gnome-terminal
+	~/.dracula/gnome-terminal/install.sh
+	if [[ `grep "~/.dir_colors/dircolors" ~/.zshrc | wc -l` = "0" ]]; then
+		echo '[ -f ~/.dir_colors/dircolors ] && eval `dircolors ~/.dir_colors/dircolors`' >> ~/.zshrc
+	fi
+	rm ~/dircolors
+fi
+
+if [[ `grep "~/.dir_colors/dircolors" ~/.zshrc | wc -l` = "0" ]]; then
+	echo "  dircolors..."
+	mkdir -p ~/.dir_colors
+	wget -O ~/.dir_colors/dircolors https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+	echo '[ -f ~/.dir_colors/dircolors ] && eval `dircolors ~/.dir_colors/dircolors`' >> ~/.zshrc
+fi
+
+command -v terminator 2>&1 >/dev/null
+if [[ $? -eq 0 ]]; then
+	echo "  terminator ..."
 	git clone https://github.com/dracula/terminator.git ~/.dracula/terminator
 	# ~/.dracula/terminator/install.sh -- skipped, already done...
 fi
