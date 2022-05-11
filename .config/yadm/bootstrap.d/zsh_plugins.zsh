484659dfde1a522a48062a6600a5fde8124c365c
Christophe Pollet <cpollet@users.noreply.github.com>
2022-05-11 20:45:34 +0200

    Add zsh plugins submodules

--- .config/yadm/bootstrap.d/zsh_plugins.zsh
+++ .config/yadm/bootstrap.d/zsh_plugins.zsh
@@ -1,4 +1,8 @@
 #!/bin/zsh
 
-yadm submodule add https://github.com/olivierverdier/zsh-git-prompt.git $HOME/.config/zsh/plugins/zsh-git-prompt
-yadm submodule add https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/zsh/plugins/zsh-syntax-highlighting
+cd $HOME
+
+# yadm submodule add https://github.com/olivierverdier/zsh-git-prompt.git .config/zsh/plugins/zsh-git-prompt
+# yadm submodule add https://github.com/zsh-users/zsh-syntax-highlighting.git .config/zsh/plugins/zsh-syntax-highlighting
+
+yadm submodule update --recursive --init
