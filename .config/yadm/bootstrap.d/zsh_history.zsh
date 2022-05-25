ca727343d2e5bb1e1852a8a6e2e498c6fb670a5d
Christophe Pollet <cpollet@users.noreply.github.com>
2022-05-25 15:06:38 +0200

    yadm - add commands to merge zsh history files

--- .config/yadm/bootstrap.d/zsh_history.zsh
+++ .config/yadm/bootstrap.d/zsh_history.zsh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+# merge history files:
+# builtin fc -R -I "$hist_file"
+# builtin fc -R -I "$another_hist_file"
+# builtin fc -W "$HISTFILE"
+
 [ -f $HOME/.zsh_history ] && [ ! -f $HOME/.config/zsh/history ] \
     && mv $HOME/.zsh_history $HOME/.config/zsh/history 
 
