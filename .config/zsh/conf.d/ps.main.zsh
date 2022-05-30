46a2a39926ff11a95382b85f86ca4575ddb2ab8d
Christophe Pollet <cpollet@users.noreply.github.com>
2022-05-30 14:53:24 +0200

    ps - add some ps_grep and ps_env

---
+++ .config/zsh/conf.d/ps.main.zsh
@@ -0,0 +1,8 @@
+alias ps_grep='ps faux | grep -i'
+function ps_env() {
+	if command -v grcat &>/dev/null; then
+		cat /proc/$1/environ | tr '\0' '\n' | grcat conf.env
+	else
+		cat /proc/$1/environ | tr '\0' '\n'
+	fi
+}
