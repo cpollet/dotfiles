2a4c709b80fab3ec25bc9397e46f902864223a62
Christophe Pollet <cpollet@users.noreply.github.com>
2022-05-30 09:17:37 +0200

    zsh - fix config

--- .config/zsh/conf.d/glow.main.zsh
+++ .config/zsh/conf.d/glow.main.zsh
@@ -1,5 +1,5 @@
 # ref: https://github.com/charmbracelet/glow
-! command -v glow &>/dev/null && exit 0
+! command -v glow &>/dev/null && return 0
 
 alias md='glow -p -w80'
 alias -s md='glow -p -w80'
