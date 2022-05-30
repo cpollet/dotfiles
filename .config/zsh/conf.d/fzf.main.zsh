dc8310ba7c2d89d283665ac8dfed6dc1fbf51c06
Christophe Pollet <cpollet@users.noreply.github.com>
2022-05-30 21:57:21 +0200

    fzf - disable preview in shell widgets

--- .config/zsh/conf.d/fzf.main.zsh
+++ .config/zsh/conf.d/fzf.main.zsh
@@ -2,6 +2,9 @@ command -v fzf &>/dev/null || return 0
 
 if command -v bat &>/dev/null; then
     export FZF_DEFAULT_OPTS="--height=50% --layout=reverse --border=none --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+    export FZF_CTRL_T_OPTS="--preview="
+    export FZF_CTRL_R_OPTS="--preview="
+    export FZF_ALT_C_OPTS="--preview="
 else
     export FZF_DEFAULT_OPTS="--height=50% --layout=reverse --border=none"
 fi
