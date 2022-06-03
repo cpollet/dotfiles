2708074893268fff932873a8b04f9a1fc89af8e3
Christophe Pollet <cpollet@users.noreply.github.com>
2022-06-03 10:17:49 +0200

    mvn - add config

--- .config/yadm/bootstrap.d/0_submodules.sh
+++ .config/yadm/bootstrap.d/0_submodules.sh
@@ -11,6 +11,8 @@ cd $HOME
 # yadm submodule add https://github.com/jeffreytse/zsh-vi-mode.git .config/zsh/plugins/zsh-vi-mode
 # yadm submodule add https://github.com/reegnz/jq-zsh-plugin.git .config/zsh/plugins/jq-zsh-plugin
 
+# yadm submodule add $DOTFILES_SECRETS_REPO .local/share/dotfiles-secrets
+
 # yadm submodule add https://github.com/TheLocehiliosan/yadm.git .local/lib/yadm
 
 yadm submodule update --recursive --init
--- .config/yadm/bootstrap.d/yadm.zsh
+++ .config/yadm/bootstrap.d/yadm.zsh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 
+ln $HOME/.local/share/dotfiles-secrets/archive $HOME/.local/share/yadm/archive
+
 $HOME/.local/bin/yadm gitconfig user.name "Christophe Pollet"
 $HOME/.local/bin/yadm gitconfig user.email "cpollet@users.noreply.github.com"
